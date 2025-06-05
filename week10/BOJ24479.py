7185e06a9e81fd4effc81bd50b3f87485844792d
Yunju07 <juile0827@naver.com>
2025-06-05 13:39:04 +0900

    [BOJ] 24479/실버2/60분
    - 재귀함수 회피

--- week10/BOJ24479.py
+++ week10/BOJ24479.py
@@ -1,35 +1,30 @@
 import sys
-
-# 알고리즘 수업 - 깊이 우선 탐색 1
 input = sys.stdin.readline
+
 N, M, R = map(int, input().split())
 
-E = {}
-visited = {}
-# 정점은 1 ~ N
-for i in range(1, N+1):
-  E[i] = []
-  visited[i] = 0
+E = {i: [] for i in range(1, N + 1)}
+visited = {i: 0 for i in range(1, N + 1)}
 
 for _ in range(M):
-  u, v = map(int, input().split())
-  E[u].append(v)
-  E[v].append(u)
+    u, v = map(int, input().split())
+    E[u].append(v)
+    E[v].append(u)
 
-for i in range(1, N+1):
-  E[i].sort()
+for i in range(1, N + 1):
+    E[i].sort(reverse=True)  
 
 order = 1
-def dfs(r):
-  global order
-  visited[r] = order
-  order += 1
-  
-  for x in E[r]:
-    if visited[x] == 0:
-      dfs(x)
-  
-dfs(R)
+stack = [R]
 
-for i in range(1, N+1):
-  print(visited[i])
+while stack:
+    node = stack.pop()
+    if visited[node] == 0:
+        visited[node] = order
+        order += 1
+        for neighbor in E[node]:
+            if visited[neighbor] == 0:
+                stack.append(neighbor)
+
+for i in range(1, N + 1):
+    print(visited[i])
