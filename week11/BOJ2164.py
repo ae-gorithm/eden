cf96e3a50eceaa106249baa87e01cff803ec4e59
Yunju07 <juile0827@naver.com>
2025-06-11 23:57:37 +0900

    [BOJ] 2164/실버4/20분
    - 큐

---
+++ week11/BOJ2164.py
@@ -0,0 +1,14 @@
+# 카드 2
+from collections import deque
+
+N = int(input())
+nums = deque()
+
+for i in range(1, N+1):
+  nums.append(i)
+
+while len(nums) > 1:
+  nums.popleft()
+  nums.append(nums.popleft())
+
+print(nums[0])
