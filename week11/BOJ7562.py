ea1ce7cb2d2cd3f2bc5c8e15720d084160df1635
Yunju07 <juile0827@naver.com>
2025-06-11 23:45:10 +0900

    [BOJ] 7562/실버1/80분
    - 방문했던 곳은 방문하지 않도록 수정 -> 시간초과 해결

--- week11/BOJ7562.py
+++ week11/BOJ7562.py
@@ -6,51 +6,43 @@
 
 # 나이트가 이동할 수 있는 0 ~ l-1 사이의 위치 
 def knight_move(r, c, l, moves, visited):
-  can_moves = [
-    [r-1, c-2], 
-    [r-2, c-1], 
-    [r-1, c+2], 
-    [r-2, c+1],
-    [r+1, c-2], 
-    [r+2, c-1], 
-    [r+1, c+2], 
-    [r+2, c+1]
-  ]
-  for m in can_moves:
-    if (0 <= m[0] < l) and (0 <= m[1] < l):
-      moves.append(m)
-  return
+    can_moves = [
+        [r-1, c-2], [r-2, c-1], [r-1, c+2], [r-2, c+1],
+        [r+1, c-2], [r+2, c-1], [r+1, c+2], [r+2, c+1]
+    ]
+    for m in can_moves:
+        nr, nc = m
+        if 0 <= nr < l and 0 <= nc < l and not visited[nr][nc]:
+            visited[nr][nc] = True
+            moves.append([nr, nc])
 
 def checking(curpoint, endpoint):
-  if (curpoint[0] == endpoint[0]) and (curpoint[1] == endpoint[1]):
-    return True
+    return curpoint[0] == endpoint[0] and curpoint[1] == endpoint[1]
 
 def bfs(l, points, end_point, count, visited):
-  moves = []
-  for cur_point in points:
-    knight_move(cur_point[0], cur_point[1], l, moves, visited)
+    moves = []
+    for cur_point in points:
+        knight_move(cur_point[0], cur_point[1], l, moves, visited)
 
-  for move in moves:
-    if checking(move, end_point):
-      return count
-  
-  # 이번 카운트에서는 없었음
-  return bfs(l, moves, end_point, count+1, visited)
+    for move in moves:
+        if checking(move, end_point):
+            return count
+
+    return bfs(l, moves, end_point, count+1, visited)
 
 T = int(input())
 
 for test_case in range(T):
-  I = int(input())
-  cur_r, cur_c = map(int, input().split())
-  end_r, end_c = map(int, input().split())
-  result = 0
-
-  if (cur_r == end_r) and (cur_c == end_c):
+    I = int(input())
+    cur_r, cur_c = map(int, input().split())
+    end_r, end_c = map(int, input().split())
     result = 0
-  else: 
-    # 이전에 방문했었던 내역 기억해야함
-    visited = [[[0] * I] for _ in range(I)]
-    result = bfs(I, [[cur_r, cur_c]], [end_r, end_c], 1, visited)
 
-  # 각 케이스별 결과 출력
-  print(result)
+    if (cur_r == end_r) and (cur_c == end_c):
+        result = 0
+    else:
+        visited = [[False] * I for _ in range(I)]
+        visited[cur_r][cur_c] = True  # 시작점 방문 처리
+        result = bfs(I, [[cur_r, cur_c]], [end_r, end_c], 1, visited)
+
+    print(result)
