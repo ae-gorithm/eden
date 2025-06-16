a32c3495c2aac5b628e16cf51065581c6d5dddb8
Yunju07 <juile0827@naver.com>
2025-06-16 20:06:02 +0900

    [BOJ] 3896/실버1/30분
    - 시간 초과

---
+++ week12/BOJ3896.py
@@ -0,0 +1,39 @@
+# 소수 사이 수열
+# 양의 정수 k 
+# 1. k가 소수인지 판단
+# 2. k랑 가장 가까운 두 소수를 구하고 차이를 구하기 ?
+
+def sosu(k):
+  for i in range(2, k):
+    if k % i == 0:
+      return False
+  return True
+
+T = int(input())
+results = []
+for test_case in range(T):
+  k = int(input())
+  
+  # 1. k가 소수인지 판단
+  if sosu(k) == True:
+    results.append(0)
+    continue
+
+  # 2. k랑 가장 가까운 두 소수를 구하고 차이를 구하기 ?
+  # 작고 가까운 소수 구하기
+  p1 = k-1
+  p2 = k+1
+  while True:
+    if sosu(p1) == True:
+      break
+    p1 -= 1
+
+  while True:
+    if sosu(p2) == True:
+      break
+    p2 += 1
+
+  results.append(p2-p1)
+   
+for result in results:
+  print(result)
