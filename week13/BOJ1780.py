13efec3102b6ab80b0c9c32295eef59a58e6e35f
Yunju07 <juile0827@naver.com>
2025-06-24 21:23:38 +0900

    [BOJ] 1780/실버2/60분
    배열 할당 -> 인덱싱으로 접근 & 재귀

--- week13/BOJ1780.py
+++ week13/BOJ1780.py
@@ -1,59 +1,49 @@
 # 종이의 개수
-
-# 종이가 다 같은 수로 되었는지 확인
-def check_paper(paper):
-  num = paper[0][0]
-  for row in paper:
-    for e in row:
-      if e != num:
-        return False
-  return True
-
-# 종이를 같은 크기의 종이 9개로 자르고 반환하기
-def divide_paper(paper, new_papers):
-  n = len(paper)
-  block = len(paper) // 3
-  
-  for i in range(0, n, block):
-    for j in range(0, n, block):
-      new_papers.append([row[j:j+block] for row in paper[i:i+block]])
-  return
-
-# 입력 시작
 N = int(input())
-papers = []
 paper = []
-
 for i in range(N):
   rows = list(map(int, input().split()))
   paper.append(rows)
-papers.append(paper)
 
-minus_one = 0
-zero = 0
-one = 0
-isBreak = False
-while True:
-  new_papers = []
-  for p in papers:
-    if check_paper(p):
-      if p[0][0] == -1:
-        minus_one += 1
-      elif p[0][0] == 0:
-        zero += 1
-      else:
-        one += 1
-      
-      if len(papers[0][0]) == 1:
-        isBreak = True
-    else:
-      divide_paper(p, new_papers)
+count = {-1: 0, 0: 0, 1: 0}
+
+# 종이가 다 같은 수로 되었는지 확인
+def check_paper(r_index, c_index, block):
+  num = paper[r_index][c_index]
+  isSame = True
   
-  if isBreak:
-    break
-  papers = new_papers
+  for r in range(r_index, r_index + block):
+    for c in range(c_index, c_index + block):
+      if paper[r][c] != num:
+        isSame = False
+        break
+      if not isSame:
+        break
+  
+  if isSame:
+    count[num] += 1
+  else:
+    new_block = block // 3
+    for n_r in range(0, block, new_block):
+      for n_c in range(0, block, new_block):
+        check_paper(r_index + n_r, c_index + n_c, new_block)
+  
 
-print(minus_one)
-print(zero)
-print(one)
+# 종이를 같은 크기의 종이 9개로 자르고 반환하기
+# def divide_paper(paper, new_papers):
+#   n = len(paper)
+#   block = len(paper) // 3
+  
+#   for i in range(0, n, block):
+#     for j in range(0, n, block):
+#       new_papers.append([row[j:j+block] for row in paper[i:i+block]])
+#   return
+
+# 인덱싱을 저장하며 재귀적으로 호출
+block = N
+check_paper(0, 0, block)
+
+print(count[-1])
+print(count[0])
+print(count[1])
       
